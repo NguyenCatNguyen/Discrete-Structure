bc18ae68dfe9540330c521a1846fb5e863293828
NguyenCatNguyen <catngn11@gmail.com>
2022-10-13 15:22:37 -0500

    chane

--- Assigment/Assigment4/Assigment4.py
+++ Assigment/Assigment4/Assigment4.py
@@ -5,18 +5,6 @@ Date: Monday October 10, 2022
 Assignment: Assignment 4
 """
 
-#Main function space (need more comment on function)
-"""
-1. Write code that determines if a relation (R) of ordered pairs is reflexive or not & then if it is not, 
-finds the reflexive closure of R (R*). The output should be:
-a) R={...}
-b) R is or is not reflexive
-c) R* if it is not reflexive
-d) Show the code works for the relation {(1,1), (4,4), (2,2), (3,3)} on the
-set {1,2,3,4}.
-e) Show the code works for the relation {(a,a), (c,c)} on the set {a,b,c,d}.
-
-"""
 
 #Functions for all the part of the assignment.
 #  
@@ -42,33 +30,209 @@ def reflexive(M,set):
     else:
         return False
 
-def composition(S, R):
-    C = []
-    #Create an empty list to hold the composition of two list.
-    for x in R:
+def transitive(M,S):
+    #For x,y,z in set s, check if (x,y) in M and (y,z) in M then (x,z) in M.
+    #Check all the value in M that satisfy the condition above.
+    count = 0
+    for x in S:
         for y in S:
-            #Loop throught all the value in S.
-            if x[1] == y[0]:
-                #if x[1] = y[0] then add the value of (x[0],y[1]) to empty list C
-                C.append((x[0], y[1]))
-    return C
+            for z in S:
+                if (x,y) in M and (y,z) in M:
+                    if (x,z) in M:
+                        count += 1
+    if count == len(S)**3:
+        return True
+    else:
+        return False
+
+#Function to check if the relation is symmetric or not.
+def symmetric(M):
+    #going through list M and check if (x,y) in M and (y,x) in M then return True.
+    #Check all the value in M that satisfy the condition above.
+    count = 0
+    for (x,y) in M:
+        if (y,x) in M:
+            count += 1
+    if count == len(M):
+        return True
+    else:
+        return False
+
+#Function to check if the relation is antisymmetric or not.
+def antisymmetric(M):
+    count = 0
+    for (x,y) in M:
+        if (y,x) in M:
+            #check if (y,x) in M, if yes then check if x = y.
+            if x == y:
+                count += 1
+    if count == 0:
+        return True
+    else:
+        return False
 
 
 
+#Function to find Reflexive Closure. 
+def CR(M,s):
+    if reflexive(M,s) == True:
+        print("b) R is reflexive\n")
+    else:
+        print("b) R is not reflexive")
+        for x in s:
+            #Adding the missing pair of (y,x) if it is missing
+            if (x,x) not in M:
+                M = list(M)
+                M.append((x,x))
+        print(f"c) R* = {M}\n")
+
+#Function to find Symmetric Closure.
+def CS(M):
+    if symmetric(M) == True:
+        print("b) R is symmetric\n")
+    else:
+        print("b) R is not symmetric")
+        for (x,y) in M:
+            if (y,x) not in M:
+                #Adding the missing pair of (y,x) if it is missing
+                M = list(M)
+                M.append((y,x))
+        print(f"c) R* = {M}\n")
+
+#Function to find Transitive Closure.
+def CT(M,s):
+    if transitive(M,s) == True:
+        print("b) R is transitive\n")
+    else:
+        print("b) R is not transitive")
+        #Using Warshall Algorithm to find R*.
+        for x in s:
+            for y in s:
+                for z in s:
+                    if (x,y) in M and (y,z) in M:
+                        M = list(M)
+                        M.append((x,z))
+        M = list(set(M))
+        print(f"c) R* = {M}\n")
+
+#Function to find Equivalence Relation.
+def CE(M,s):
+    #Check if R is reflexive
+    if reflexive(M,s) == True:
+        if symmetric(M) == True:
+            if transitive(M,s) == True:
+                print("b) R is an equivalence relation\n")
+            else:
+                print("b) R is not an equivalence relation, because it is not transitive\n")
+        else:
+            print("b) R is not an equivalence relation, because it is not symmetric\n")
+    else:
+        print("b) R is not an equivalence relation, because it is not reflexive\n")
+
+#Function to find Poset.
+def CP(M,s):
+    if reflexive(M,s) == True:
+        if antisymmetric(M) == True:
+            if transitive(M,s) == True:
+                print("b) R is a poset\n")
+            else:
+                print("b) R is not a poset, because it is not transitive\n")
+        else:
+            print("b) R is not a poset, because it is not antisymmetric\n")
+    else:
+        print("b) R is not a poset, because it is not reflexive\n")
+
+
+
+#Functiont to execute each part of the assignment.
+def Part1(R,s):
+    print(f"For relationship R= {R} on the set {s}")
+    print("a) R order pair = ", order_pair(R))
+    CR(R,s)
+   
+
+def Part2(R,s):
+    print(f"For relationship R= {R} on the set {s}")
+    print("a) R order pair = ", order_pair(R))
+    CS(R)
+ 
+
+def Part3(R,se):
+    print(f"For relationship R= {R} on the set {se}")
+    print("a) R order pair = ", order_pair(R))
+    CT(R,se)
+
+def Part4(R,s):
+    print(f"For relationship R= {R} on the set {s}")
+    print("a) R order pair = ", order_pair(R))
+    CE(R,s)
+
+def Part5(R,s):
+    print(f"For relationship R= {R} on the set {s}")
+    print(f"a) S = {s}")
+    print(f"b) R = {R}")
+    CP(R,s)
+
+
+
+#Assigment excercute.
 #Part 1
 set1 = {1,2,3,4}
 relation1 = {(1,1), (4,4), (2,2), (3,3)}
-
-print(f"For relationship R={relation1} on the set {set1}")
-print("a) R = ", order_pair(relation1))
-print("b) R is reflexive: ",reflexive(relation1,set1))
-
-set2 = {a,b,c,d}
-relation2 = {(a,a), (c,c)}
-print(f"For relationship R={relation2} on the set {set2}")
-print("a) R = ", order_pair(relation2))
-print("b) R is reflexive: ",reflexive(relation2,set2))
+set2 = {"a","b","c","d"}
+relation2 = {("a","a"), ("c","c")}
+print("\nPart 1 - Reflexive")
+print("1.d")
+Part1(relation1,set1)
+print("1.e")
+Part1(relation2,set2)
+print("-"*70)
 
 
+#Part 2
+relation3 = {(1,2), (4,4), (2,1), (3,3)}
+relation4 = {(1,2),(3,3)}
+print("\nPart 2 - Symmetric")
+print("2.d")
+Part2(relation3,set1)
+print("2.e")
+Part2(relation4,set1)
+print("-"*70)
+
+#Part 3
+relation5 = {("a","b"),("d","d"),("b","c"),("a","c")}
+set6 = {1,2,3}
+relation6 = {(1,1),(1,3),(2,2),(3,1),(3,2)}
+print("\nPart 3 - Transitive")
+print("3.d")
+Part3(relation5,set2)
+print("3.e")
+Part3(relation6,set6)
+print("-"*70)
+
+
+#Part 4
+relation7 = {(1,1),(2,2),(2,3)}
+set7 = {1,2,3}
+relation8 = {("a","a"),("b","b"),("c","c"),("b","c"),("c","b")}
+set8 = {"a","b","c"}
+print("\nPart 4 - Equivalence")
+print("4.d")
+Part4(relation7,set7)
+print("4.e")
+Part4(relation8,set8)
+print("-"*70)
+
+#Part 5[ ]
+relation9 = {(1,1), (1,2), (2,2), (3,3), (4,1), (4,2), (4,4)}
+set9 = {1,2,3,4}
+relation10 = {(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 1), (1, 2), (1, 3), (2, 0), (2, 2), (3, 3)}
+set10 = {0, 1, 2, 3}
+print("\nPart 5 - Poset")
+print("5.d")
+Part5(relation9,set9)
+print("5.e")
+Part5(relation10,set10)
+print("-"*70)
 
     
--- Test.py
+++ Test.py
@@ -1,6 +1,129 @@
-num = [3,2,1,4,2]
-print(len(num) )
+def FiveD():
+	A = {1,2,3,4}
+	R = [(1,1), (1,2), (2,2), (3,3), (4,1), (4,2), (4,4)]
+	def transitive(R,A):
+		for a in A:
+			for b in A:
+				if (a, b) in R:
+					for c in A:
+						if (b, c) in R and (a, c) not in R:
+							return False
+		return True
+	def antisymmetric(R,A):
+		for ele1, ele2 in R:# for loop that iterates over the ordered pairs in the relation above
+			if (ele2, ele1) not in R:#if statement with the condition that the ordered pairs in R must be antisymmetric to excute further code
+				return True 
+		return False
+	def reflexive(R,a):
+		for ele in a:
+			if (ele, ele) not in R:
+				return False
+		return True
+	print(f"5d)")
+	print(f"\ta) S = {A}")
+	print(f"\tb) R = {R}")
+	if transitive(R,A) == True and antisymmetric(R,A) == True and reflexive(R,A) == True:
+		print (f'\tc) (S,R) is a poset')
+	else:
+		print(f'\tc) (S,R) is not a poset')
+		print(f'\td) Reasons (S,R) is not a poset: ')
+	if transitive(R,A) == False:
+		print('\t\tIt is not transitive')
+	if reflexive(R,A) == False:
+		print('\t\tIt is not reflexive')
+	if antisymmetric(R,A) == False:
+		print('\t\tIt is not antisymmetric')
+	print('-------------------------------------------------------')# prints a line to help seperate each problem
+FiveD()
 
-while 4:
-    print("hello")
-    
+def FiveE():
+	A = {0,1,2,3}
+	R = [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 1), (1, 2), (1, 3), (2, 0), (2, 2), (3, 3)]
+	def transitive(R,A):
+		for a in A:
+			for b in A:
+				if (a, b) in R:
+					for c in A:
+						if (b, c) in R and (a, c) not in R:
+							return False
+		return True
+	def antisymmetric(R,A):
+		for ele1, ele2 in R:# for loop that iterates over the ordered pairs in the relation above
+			if (ele2, ele1) not in R:#if statement with the condition that the ordered pairs in R must be antisymmetric to excute further code
+				return True 
+		return False
+	def reflexive(R,a):
+		for ele in a:
+			if (ele, ele) not in R:
+				return False
+		return True
+	print(f"5e)")
+	print(f"\ta) S = {A}")
+	print(f"\tb) R = {R}")
+	if transitive(R,A) == True and antisymmetric(R,A) == True and reflexive(R,A) == True:
+		print (f'\tc) (S,R) is a poset')
+	else:
+		print(f'\tc) (S,R) is not a poset')
+		print(f'\td) Reasons (S,R) is not a poset: ')
+	if transitive(R,A) == False:
+		print('\t\tR is not transitive')
+	if reflexive(R,A) == False:
+		print('\t\tR is not reflexive')
+	if antisymmetric(R,A) == False:
+		print('\t\tR is not symmetric')
+	print('-------------------------------------------------------')# prints a line to help seperate each problem
+FiveE()
+
+
+
+"""
+1. Write code that determines if a relation (R) of ordered pairs is reflexive or not & then if it is not, 
+finds the reflexive closure of R (R*). The output should be:
+a) R={...}
+b) R is or is not reflexive
+c) R* if it is not reflexive
+d) Show the code works for the relation {(1,1), (4,4), (2,2), (3,3)} on the
+set {1,2,3,4}.
+e) Show the code works for the relation {(a,a), (c,c)} on the set {a,b,c,d}.
+
+2. Write code that determines if a relation (R) of ordered pairs is symmetric or 
+not & then if it is not, finds the symmetric closure of R (R*). The output should be:
+a) R={...}
+b) R is or is not symmetric
+c) R* if it is not symmetric
+d) Show the code works for the relation {(1,2), (4,4), (2,1), (3,3)} on the
+set {1,2,3,4}.
+e) Show the code works for the relation {(1,2), (3,3)} on the set
+{1,2,3,4}.
+
+3. Write code that determines if a relation (R) of ordered pairs is transitive or not
+& then if it is not, finds the transitive closure of R (R*) using Warshalls Algorithm. The output should be:
+a) R={...}
+b) R is or is not transitive 
+c) R* if it is not transitive
+d) Show the code works for the relation {(a,b), (d,d), (b,c), (a,c)} on the set {a,b,c,d}.
+e) Show the code works for the relation {(1,1),(1,3),(2,2),(3,1),(3,2)} on the set {1,2,3}.
+
+4. Write code that determines if a relation (R) of ordered pairs is an equivalence 
+relation or not and the reason why. The output should be:
+a) R={...}
+b) R is or is not an equivalence relation
+c) The reasons why, if it is not an equivalence relation (i.e., it is not
+reflexive, and/or it is not symmetric, and/or it is not transitive).
+d) Show the code works for the relation {(1,1),(2,2),(2,3)} on the set
+{1,2,3}.
+e) Show the code works for the relation {(a,a),(b,b),(c,c),(b,c),(c,b)} on
+the set {a,b,c}.
+
+5. Write code that determines if a relation (R) of ordered pairs is a poset of the
+set (S) or not and the reason why. The output should be: a) S={...}
+b) R={...}
+c) (S,R) is or is not a poset
+d) The reason why, if it is not poset (i.e., it is not reflexive, and/or it is
+not antisymmetric, and/or it is not transitive.
+e) Show the code works for the relation {(1,1), (1,2), (2,2), (3,3), (4,1),
+(4,2), (4,4)} on the set {1, 2, 3, 4}.
+f) Show the code works for the relation {(0, 0), (0, 1), (0, 2), (0, 3), (1,
+0), (1, 1), (1, 2), (1, 3), (2, 0), (2, 2), (3, 3)} on the set {0, 1, 2, 3}.
+
+"""
