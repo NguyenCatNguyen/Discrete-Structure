513706d6858a2c791b772287feb8b6fb86c000f7
NguyenCatNguyen <catngn11@gmail.com>
2022-10-25 23:44:31 -0500

    Complete assigment

--- Assigment/Assigment5/Assigment5.py
+++ Assigment/Assigment5/Assigment5.py
@@ -1,33 +1,4 @@
 """
-You may use any language you want, but if you want help from me or one of the 
-SIs, you should probably use C++ or Python. 
-• Recall that a function f from A to B, is a relation f ⊆ A x B such that each element 
-of A is assigned to exactly one element of B. 
-• Thus, we can represent a function as a relation of order pairs, (a,b), where a ∊ A, b 
-∊ B, and b = f(a). 
-• Write a program that takes inputs of A, B, and a relation f and 
-a) determines if the relation is a function or not, 
-b) and if it is a function, determines if it is injective, surjective, or bijective, 
-c) and if it is bijective, determines the inverse function. 
-• Your program should print out the inputs using the notation you learned in class, 
-i.e., A = {...}, B = {...}, f = {(a,b), ...}, whether the relation is a function or not, 
-b) and if it is a function, whether it is injective, surjective, or bijective, c) and if it 
-is bijective, what is the inverse function. Test your program with the following 
-inputs: 
-1. A = {a,b,c,d}, B = {v,w,x,y,z}, f = {(a,z),(b,y),(c,x),(d,w)} 
-2. A = {a,b,c,d}, B = {x,y,z}, f = {(a,z),(b,y),(c,x),(d,z)} 
-3. A = {a,b,c,d}, B = {w,x,y,z}, f = {(a,z),(b,y),(c,x),(d,w)} 
-4. A = {a,b,c,d}, B = {1,2,3,4,5}, f = {(a,4),(b,5),(c,1),(d,3)} 
-5. A = {a,b,c}, B = {1,2,3,4}, f = {(a,3),(b,4),(c,1)} 
-6. A = {a,b,c,d}, B = {1,2,3}, f = {(a,2),(b,1),(c,3),(d,2)} 
-7. A = {a,b,c,d}, B = {1,2,3,4}, f = {(a,4),(b,1),(c,3),(d,2)} 
-8. A = {a,b,c,d}, B = {1,2,3,4}, f = {(a,2),(b,1),(c,2),(d,3)} 
-9. A = {a,b,c}, B = {1,2,3,4}, f = {(a,2),(b,1),(a,4),(c,3)} 
-• Provide comments that explain what each line of code is doing. See rubric below. 
-
-"""
-
-"""
 Authour: Nguyen Cat Nguyen
 KUID: 3077463
 Date: Tuesday October 25, 2022
@@ -35,18 +6,128 @@ Assignment: Assignment 5
 """
 
 #Function to check if the relation is a function or not.
-def check_function(A,B,F):
-    for x,y in A,B:
-        if (x,y) in F:
-            true_count += 1
-    if true_count == len(F):
+def check_function(A,B,f):
+    count = 0
+    for x in A:
+        for y in B:
+            if (x,y) in f:
+                count += 1
+    if count == len(A):
         return True
     else:
         return False
     
-#Function to check if the relation is injective or not.
-def check_injective(A,B,F):
-    #
+#Function to check if the relation is an injective or not.
+def check_injective(A,B,f):
+    List = []
+    for (x,y) in f:
+        for x in (x,y):
+            List.append(x)
+    #Check if value in List is unique
+    if len(List) == len(set(List)):
+        return True
+    else:
+        return False
+
+#Function to check if the relation is surjective or not.
+def check_surjective(B,f):
+    count = 0
+    for (x,y) in f:
+        for y in (x,y):
+            #Check if y is in B
+            if y in B:
+                count += 1
+    if count == len(B):
+        #Check if all y values in f are in B.
+        return True
+    else:
+        return False
+
+#Function to check if the relation is bijective or not.
+def check_bijective(A,B,f):
+    #A function is bijective if and only if it is both injective and surjective.
+    if check_function(A,B,f) == True and check_injective(A,B,f) == True and check_surjective(B,f) == True:
+        return True
+    else:
+        return False
+
+def check_inverse(A,B,f):
+    L = []
+    for (x,y) in f:
+        L.append((y,x))
+    print(f"c) The inverse of f is: {L}")
+
+
+#Main function to printout all the require outcome. 
+def main(A,B,f,num):
+    print(f"{num}. A = {A}, B = {B}, f = {f}")
+    if check_function(A,B,f) == True:
+        print("a) The relation is a function.")
+        print("b) The relation is:")
+        print(f"The relation is injective: {check_injective(A,B,f)}")
+        print(f"The relation is surjective: {check_surjective(B,f)}")
+        if check_bijective(A,B,f) == True:
+            print("The relation is bijective.")
+            check_inverse(A,B,f)
+    else:
+        print("a) The relation is not a function.")
+
+#Test case
+#1
+A = ["a","b","c","d"] 
+B = ["v","w","x","y","z"]
+f = [("a","z"),("b","y"),("c","x"),("d","w")]
+#2
+B2 = ["x","y","z"]
+f2 = [("a","z"),("b","y"),("c","x"),("d","z")]
+#3
+B3 = ["w","x","y","z"]
+#4
+B4 = {1,2,3,4,5}
+f4 = {("a",4),("b",5),("c",1),("d",3)} 
+#5
+A5 = ["a","b","c"]
+B5 = {1,2,3,4}
+f5 = {("a",3),("b",4),("c",1)}
+#6
+f6 = {("a",2),("b",1),("c",3),("d",2)}
+B6 = {1,2,3,4}
+#7
+f7 = {("a",4),("b",1),("c",3),("d",2)}
+B7 = {1,2,3,4}
+#8
+f8 = {("a",2),("b",1),("c",2),("d",3)}
+#9
+A9 = ["a","b","c"]
+B9 = {1,2,3,4}
+f9 = {("a",2),("b",1),("a",4),("c",3)}
+#Question 1
+main(A,B,f,1)
+
+#Question 2
+main(A,B2,f2,2)
+
+#Question 3
+main(A,B3,f,3)
+
+#Question 4
+main(A,B4,f4,4)
+
+#Question 5
+main(A5,B5,f5,5)
+
+#Question 6
+main(A,B6,f6,6)
+
+#Question 7
+main(A,B5,f7,7)
+
+#Question 8
+main(A5,B6,f8,8)
+
+#Question 9
+main(A9,B9,f9,9)
+
 
 
 
